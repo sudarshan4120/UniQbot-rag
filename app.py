46b7436dd5de2ce408f7ccf86683065a3326f37b
namansnghl <48488518+namansnghl@users.noreply.github.com>
2025-03-18 16:08:32 -0400

    claude integrated with app

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ import utils  # This loads vars, do not remove
 
 import scrapper
 import preprocessing
+import model
 
 
-def run():
+def run_data_pipeline():
     scrapper.run_scrapper()
     preprocessing.run_cleaner()
 
 
 if __name__ == "__main__":
-    run()
+    # run_data_pipeline()
+    model.run_rag_claude()
